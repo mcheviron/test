b1ddd97e2285ef74152444d01fbcb64b0ec4e588
Mostafa Elataby-Cheviron <mostafaabdelhameed@outlook.com>
2024-03-15 20:00:27 +0100

    Refactor logger and error handling in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"test/internal/logger"
 	"test/internal/server"
 )
 
 func main() {
-
-	server := server.NewServer()
-
-	err := server.ListenAndServe()
+	logger, err := logger.NewLogger(logger.LoggerConfig{
+		LoggerType: "text",
+		Level:      slog.LevelDebug,
+		AddSource:  true,
+	})
 	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
+		log.Fatalf("cannot create logger: %s. Cannot initialise...", err)
+	}
+
+	server, err := server.NewServer(logger)
+	if err != nil {
+		logger.Error("cannot create server", slog.String("error", err.Error()))
+		logger.Info("Exiting...")
+		os.Exit(1)
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		logger.Error("cannot start server", slog.String("error", err.Error()))
+		logger.Info("Exiting...")
+		os.Exit(1)
 	}
 }
